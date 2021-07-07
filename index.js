a07630adb9652f807e2c728559341704186d5455
AJAY627 <ajay41527@gmail.com>
2021-07-07 16:34:58 +0530

    fair up express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in runing the server:${err}`);
+    }
+    console.log(`Server is runing on port:,${port}`);
+});

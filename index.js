27d27b938630eceee82bcaffed4d42c306dbae31
AJAY627 <ajay41527@gmail.com>
2021-07-08 09:24:41 +0530

    conect mongoDB using mongoose

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./cofig/mongoose');
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 
 // user express router
